75a7b742068ad4c5be09eefdb8cf41197ba24cd4
MuhammadBek97 <allayevmuhammad8@gmail.com>
2025-09-26 05:10:13 +0500

    Fix SSL connection for password update script

--- update_admin1_password.js
+++ update_admin1_password.js
@@ -4,7 +4,9 @@ const { Pool } = require('pg');
 // Production database connection
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 async function updateAdmin1Password() {
