c14a6c0cd6e28a4140d6eee5f7ca825891db75ba
MuhammadBek97 <allayevmuhammad8@gmail.com>
2025-09-28 15:31:46 +0500

    Fix CORS: Add localhost:5186 to allowed origins

--- server.js
+++ server.js
@@ -74,10 +74,12 @@ const corsOptions = {
             'http://localhost:5173',
             'http://localhost:5174',
             'http://localhost:5181',
+            'http://localhost:5186',
             'http://localhost:8080',
             'http://127.0.0.1:5173',
             'http://127.0.0.1:5174',
-            'http://127.0.0.1:5181'
+            'http://127.0.0.1:5181',
+            'http://127.0.0.1:5186'
         ];
         
         // Origin yo'q bo'lsa (masalan, Postman) yoki ruxsat etilgan origin bo'lsa
